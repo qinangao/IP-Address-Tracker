580eefd0834f01f395cfd09dd6d7e33d7733d637
qinanagao@gmail.com <qinanagao@gmail.com>
2025-05-05 22:43:22 +0100

    refactor Header component structure and styling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,12 @@
 import Input from "./Input";
+import Result from "./Result";
+import Title from "./Title";
 function Header() {
   return (
-    <div className="bg-[url(/assets/pattern-bg-mobile.png)] p-[10px] text-3xl md:bg-[url(/assets/pattern-bg-desktop.png)] bg-cover bg-no-repeat md:h-[350px] flex flex-col">
-      <h1 className="py-[20px] md:p-[30px]  flex items-center justify-center text-white md:text-4xl font-bold tracking-[1px]">
-        IP Address Tracker
-      </h1>
+    <div className="relative bg-[url(/assets/pattern-bg-mobile.png)] p-[10px] text-3xl md:bg-[url(/assets/pattern-bg-desktop.png)] bg-cover bg-no-repeat md:h-[350px] flex flex-col">
+      <Title />
       <Input />
+      <Result />
     </div>
   );
 }
