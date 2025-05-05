99d7ee0baf2854ea6341f8016ef8b2e6da130a21
qinanagao@gmail.com <qinanagao@gmail.com>
2025-05-05 22:43:34 +0100

    add Title component for displaying the IP Address Tracker header

---
+++ src/components/Title.tsx
@@ -0,0 +1,9 @@
+function Title() {
+  return (
+    <h1 className="py-[20px] md:p-[30px]  flex items-center justify-center text-white md:text-4xl font-bold tracking-[1px]">
+      IP Address Tracker
+    </h1>
+  );
+}
+
+export default Title;
