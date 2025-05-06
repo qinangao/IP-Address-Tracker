2de62f424c29372e3867e8e0e3a13d2ced800e44
qinanagao@gmail.com <qinanagao@gmail.com>
2025-05-06 20:28:56 +0100

    fix(Header): adjust height for mobile view in Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import Result from "./Result";
 import Title from "./Title";
 function Header() {
   return (
-    <div className="relative bg-[url(/assets/pattern-bg-mobile.png)] p-[10px] text-3xl md:bg-[url(/assets/pattern-bg-desktop.png)] bg-cover bg-no-repeat md:h-[350px] flex flex-col">
+    <div className="relative h-[300px] bg-[url(/assets/pattern-bg-mobile.png)] p-[10px] text-3xl md:bg-[url(/assets/pattern-bg-desktop.png)] bg-cover bg-no-repeat md:h-[350px] flex flex-col">
       <Title />
       <Input />
       <Result />
