4da75897c22f43084a923fc080cb1b01323e9a1b
qinanagao@gmail.com <qinanagao@gmail.com>
2025-05-06 22:34:34 +0100

    update layout structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Header from "./components/Header";
+import Map from "./components/Map";
 
 function App() {
   return (
-    <div>
+    <div className="h-screen w-screen">
       <Header />
+      <Map />
     </div>
   );
 }
