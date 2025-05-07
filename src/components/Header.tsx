ced30ac0fc35fef64891e76bd4853bd5c5582e5c
qinanagao@gmail.com <qinanagao@gmail.com>
2025-05-07 21:24:16 +0100

    fix(Result): add loading state and integrate location context

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,7 @@
 import Input from "./Input";
 import Result from "./Result";
 import Title from "./Title";
+
 function Header() {
   return (
     <div className="relative h-[300px] bg-[url(/assets/pattern-bg-mobile.png)] p-[10px] text-3xl md:bg-[url(/assets/pattern-bg-desktop.png)] bg-cover bg-no-repeat md:h-[350px] flex flex-col">
--- src/components/Result.tsx
+++ src/components/Result.tsx
@@ -1,3 +1,4 @@
+import { useLocation } from "../contexts/locationContext";
 // Reusable DataDisplay component
 function DataDisplay({ label, value }: { label: string; value: string }) {
   return (
@@ -9,12 +10,19 @@ function DataDisplay({ label, value }: { label: string; value: string }) {
 }
 
 function Result() {
+  const { location } = useLocation();
+  if (!location) {
+    return <p>{"Loading..."}</p>;
+  }
   return (
-    <div className="absolute z-100 top-45 md:top-55  left-1/2 transform -translate-x-1/2  md:h-[160px] bg-white w-[90%] xl:w-[65%] 2xl:w-[55%] p-[20px] text-center rounded-xl flex flex-col gap-[20px] md:flex-row  lg:text-left md:gap-0 md:px-0  lg:divide-x lg:divide-gray-300">
-      <DataDisplay label="IP ADDRESS" value="192.212.233.43" />
-      <DataDisplay label="LOCATION" value="Brooklyn, NY" />
-      <DataDisplay label="TIMEZONE" value="UTC-05:00" />
-      <DataDisplay label="ISP" value="SpaceX Starlink" />
+    <div className="absolute z-100 top-45 md:top-55  left-1/2 transform -translate-x-1/2  md:h-[160px] bg-white w-[90%] xl:w-[65%] 2xl:w-[60%] p-[20px] text-center rounded-xl flex flex-col gap-[20px] md:flex-row  lg:text-left md:gap-0 md:px-0  lg:divide-x lg:divide-gray-300">
+      <DataDisplay label="IP ADDRESS" value={location?.ip} />
+      <DataDisplay
+        label="LOCATION"
+        value={`${location?.city}, ${location?.country} ${location?.postcode}`}
+      />
+      <DataDisplay label="TIMEZONE" value={`UTC ${location.timezone}`} />
+      <DataDisplay label="ISP" value={location.isp} />
     </div>
   );
 }
