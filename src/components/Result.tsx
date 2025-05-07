1fbdf6ae94e7091428bcab0c58bc15288ac5570c
qinanagao@gmail.com <qinanagao@gmail.com>
2025-05-07 21:32:12 +0100

    add(DataDisplay): create reusable DataDisplay component

--- src/components/Result.tsx
+++ src/components/Result.tsx
@@ -1,13 +1,5 @@
 import { useLocation } from "../contexts/locationContext";
-// Reusable DataDisplay component
-function DataDisplay({ label, value }: { label: string; value: string }) {
-  return (
-    <div className="bg-white font-semibold flex flex-col items-center md:pr-7 pl-3 lg:items-start lg:pr-15 lg:pl-5">
-      <p className="text-[15px] text-[#969696] tracking-[1px] ">{label}</p>
-      <p className="pt-[10px] md:pt-[20px] text-2xl">{value}</p>
-    </div>
-  );
-}
+import DataDisplay from "./DataDisplay";
 
 function Result() {
   const { location } = useLocation();
