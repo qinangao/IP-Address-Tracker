b88652100f57dc25a79d7f5db257d3e819e41888
qinanagao@gmail.com <qinanagao@gmail.com>
2025-05-08 23:05:42 +0100

    refactor(Result): adjust width of Result component for better responsiveness

--- src/components/Result.tsx
+++ src/components/Result.tsx
@@ -6,7 +6,7 @@ function Result() {
   if (!location) return;
 
   return (
-    <div className="absolute z-100 top-45 md:top-55  left-1/2 transform -translate-x-1/2  md:h-[160px] bg-white w-[90%] xl:w-[65%] 2xl:w-[60%] p-[20px] text-center rounded-xl flex flex-col gap-[20px] md:flex-row  lg:text-left md:gap-0 md:px-0  lg:divide-x lg:divide-gray-300">
+    <div className="absolute z-100 top-45 md:top-55  left-1/2 transform -translate-x-1/2  md:h-[160px] bg-white w-[90%] xl:w-[65%] 2xl:w-[53%] p-[20px] text-center rounded-xl flex flex-col gap-[20px] md:flex-row  lg:text-left md:gap-0 md:px-0  md:divide-x lg:divide-gray-300">
       <DataDisplay label="IP ADDRESS" value={location?.ip} />
       <DataDisplay
         label="LOCATION"
