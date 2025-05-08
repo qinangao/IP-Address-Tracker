4532ace1299df972aafe58beeceb7886b03c3bcd
qinanagao@gmail.com <qinanagao@gmail.com>
2025-05-08 22:00:47 +0100

    refactor(Result): simplify loading state handling in Result component

--- src/components/Result.tsx
+++ src/components/Result.tsx
@@ -3,9 +3,8 @@ import DataDisplay from "./DataDisplay";
 
 function Result() {
   const { location } = useLocation();
-  if (!location) {
-    return <p>{"Loading..."}</p>;
-  }
+  if (!location) return;
+
   return (
     <div className="absolute z-100 top-45 md:top-55  left-1/2 transform -translate-x-1/2  md:h-[160px] bg-white w-[90%] xl:w-[65%] 2xl:w-[60%] p-[20px] text-center rounded-xl flex flex-col gap-[20px] md:flex-row  lg:text-left md:gap-0 md:px-0  lg:divide-x lg:divide-gray-300">
       <DataDisplay label="IP ADDRESS" value={location?.ip} />
