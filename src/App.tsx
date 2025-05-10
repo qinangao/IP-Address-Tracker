694b3374470e637932d99c78a8ab4586ba25a46a
qinanagao@gmail.com <qinanagao@gmail.com>
2025-05-10 10:15:25 +0100

    adjust div width to full

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import Header from "./components/Header";
 import Tracker from "./components/Tracker";
+import Footer from "./components/Footer";
 import { LocationProvider } from "./contexts/locationContext";
 
 function App() {
   return (
-    <div className="h-screen w-screen">
+    <div className="h-screen w-full">
       <LocationProvider>
         <Header />
         <Tracker />
+        <Footer />
       </LocationProvider>
     </div>
   );
