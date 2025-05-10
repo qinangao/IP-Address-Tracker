bbb99d81bad9ceffd2fcd8dc7089fb9f72f9190a
qinanagao@gmail.com <qinanagao@gmail.com>
2025-05-10 18:52:13 +0100

    update padding for Title component to enhance visual spacing

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,6 +1,6 @@
 function Title() {
   return (
-    <h1 className="py-[20px] md:p-[30px]  flex items-center justify-center text-white md:text-4xl font-bold tracking-[1px]">
+    <h1 className="py-[20px] md:p-[40px]  flex items-center justify-center text-white md:text-4xl font-bold tracking-[1px]">
       IP Address Tracker
     </h1>
   );
