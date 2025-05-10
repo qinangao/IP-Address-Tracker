f2fbc1f1fd6ca9747eafc8ab89c32501e94ab27e
qinanagao@gmail.com <qinanagao@gmail.com>
2025-05-10 18:52:07 +0100

    refactor Result component to improve data display and styling

--- src/components/Result.tsx
+++ src/components/Result.tsx
@@ -6,14 +6,19 @@ function Result() {
   if (!location) return;
 
   return (
-    <div className="absolute z-10 top-45 md:top-55  left-1/2 transform -translate-x-1/2  md:h-[160px] bg-white w-[90%] xl:w-[65%] 2xl:w-[53%] p-[20px] text-center rounded-xl flex flex-col gap-[20px] md:flex-row  lg:text-left md:gap-0 md:px-0  md:divide-x lg:divide-gray-300">
+    <div className="absolute z-10 top-45 md:top-65  left-1/2 transform -translate-x-1/2  md:h-[160px] bg-white w-[90%] xl:w-[65%] 2xl:w-[55%] p-[20px] text-center rounded-xl flex flex-col gap-[20px] md:flex-row  lg:text-left md:gap-0 md:px-0  md:divide-x lg:divide-gray-300">
       <DataDisplay label="IP ADDRESS" value={location?.ip} />
       <DataDisplay
         label="LOCATION"
-        value={`${location?.city}, ${location?.country} ${location?.postcode}`}
+        value={`${location?.city}, ${location?.countryName}`}
       />
-      <DataDisplay label="TIMEZONE" value={`UTC ${location.timezone}`} />
-      <DataDisplay label="ISP" value={location.isp} />
+      <DataDisplay label="COUNTRY" value={`${location.region}`} />
+      <DataDisplay
+        label="CALLING CODE"
+        value={location.callingCode}
+        imgSrc={location.flag}
+        imgAlt={location.countryCode}
+      />
     </div>
   );
 }
