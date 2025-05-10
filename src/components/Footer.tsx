171476606b7d64ffbec49a152fccc5608329607b
qinanagao@gmail.com <qinanagao@gmail.com>
2025-05-10 10:15:34 +0100

    add footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+function Footer() {
+  return (
+    <div className="bg-[#556bd1] text-white md:text-xl text-center py-4">
+      Developed by <a href="https://qinangao.netlify.app/">Qinan Gao</a>, with
+      UI design sourced from
+      <a href="https://www.frontendmentor.io/"> Frontend Mentor</a>.
+    </div>
+  );
+}
+
+export default Footer;
